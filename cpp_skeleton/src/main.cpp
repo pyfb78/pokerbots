5fb60abd89e5d58d3027b081c86d6516e26cf5f8
gaurab1 <gaurabdps@gmail.com>
2025-01-06 11:58:34 -0600

    Add supporting bounty API for cpp

--- cpp_skeleton/src/main.cpp
+++ cpp_skeleton/src/main.cpp
@@ -13,12 +13,15 @@ struct Bot {
     @param roundState The RoundState object.
     @param active Your player's index.
   */
-  void handleNewRound(GameInfoPtr gameState, RoundStatePtr roundState, int active) {
-    // int myBankroll = gameState->bankroll;  // the total number of chips you've gained or lost from the beginning of the game to the start of this round
-    // float gameClock = gameState->gameClock;  // the total number of seconds your bot has left to play this game
-    // int roundNum = gameState->roundNum;  // the round number from 1 to State.NUM_ROUNDS
-    // auto myCards = roundState->hands[active];  // your cards
-    // bool bigBlind = (active == 1);  // true if you are the big blind
+  void handleNewRound(GameInfoPtr gameState, RoundStatePtr roundState,
+                      int active) {
+    // int myBankroll = gameState->bankroll;  // the total number of chips
+    // you've gained or lost from the beginning of the game to the start of this
+    // round float gameClock = gameState->gameClock;  // the total number of
+    // seconds your bot has left to play this game int roundNum =
+    // gameState->roundNum;  // the round number from 1 to State.NUM_ROUNDS auto
+    // myCards = roundState->hands[active];  // your cards bool bigBlind =
+    // (active == 1);  // true if you are the big blind
   }
 
   /*
@@ -28,14 +31,20 @@ struct Bot {
     @param terminalState The TerminalState object.
     @param active Your player's index.
   */
-  void handleRoundOver(GameInfoPtr gameState, TerminalStatePtr terminalState, int active) {
-    // int myDelta = terminalState->deltas[active];  // your bankroll change from this round
-    // auto previousState = std::static_pointer_cast<const RoundState>(terminalState->previousState);  // RoundState before payoffs
-    // int street = previousState->street;  // 0, 3, 4, or 5 representing when this round ended
-    // auto myCards = previousState->hands[active];  // your cards
-    // auto oppCards = previousState->hands[1-active];  // opponent's cards or "" if not revealed
+  void handleRoundOver(GameInfoPtr gameState, TerminalStatePtr terminalState,
+                       int active) {
+    // int myDelta = terminalState->deltas[active];  // your bankroll change
+    // from this round auto previousState = std::static_pointer_cast<const
+    // RoundState>(terminalState->previousState);  // RoundState before payoffs
+    // int street = previousState->street;  // 0, 3, 4, or 5 representing when
+    // this round ended auto myCards = previousState->hands[active];  // your
+    // cards auto oppCards = previousState->hands[1-active];  // opponent's
+    // cards or "" if not revealed bool myBountyHit =
+    // terminalState->bounty_hits[active];  // true if your bounty hit this
+    // round bool oppBountyHit = terminalState->bounty_hits[1-active];  // true
+    // if your opponent's bounty hit this round
   }
-  
+
   /*
     Where the magic happens - your code should implement this function.
     Called any time the engine needs an action from your bot.
@@ -45,24 +54,33 @@ struct Bot {
     @param active Your player's index.
     @return Your action.
   */
-  Action getAction(GameInfoPtr gameState, RoundStatePtr roundState, int active) {
-    auto legalActions = roundState->legalActions();  // the actions you are allowed to take
-    // int street = roundState->street;  // 0, 3, 4, or 5 representing pre-flop, flop, turn, or river respectively
-    // auto myCards = roundState->hands[active];  // your cards
-    // auto boardCards = roundState->deck;  // the board cards
-    // int myPip = roundState->pips[active];  // the number of chips you have contributed to the pot this round of betting
-    // int oppPip = roundState->pips[1-active];  // the number of chips your opponent has contributed to the pot this round of betting
-    // int myStack = roundState->stacks[active];  // the number of chips you have remaining
-    // int oppStack = roundState->stacks[1-active];  // the number of chips your opponent has remaining
-    // int continueCost = oppPip - myPip;  // the number of chips needed to stay in the pot
-    // int myContribution = STARTING_STACK - myStack;  // the number of chips you have contributed to the pot
-    // int oppContribution = STARTING_STACK - oppStack;  // the number of chips your opponent has contributed to the pot
-    // if (legalActions.find(Action::Type::RAISE) != legalActions.end()) {
-    //   auto raiseBounds = roundState->raiseBounds();  // the smallest and largest numbers of chips for a legal bet/raise
-    //   int minCost = raiseBounds[0] - myPip;  // the cost of a minimum bet/raise
-    //   int maxCost = raiseBounds[1] - myPip;  // the cost of a maximum bet/raise
+  Action getAction(GameInfoPtr gameState, RoundStatePtr roundState,
+                   int active) {
+    auto legalActions =
+        roundState->legalActions(); // the actions you are allowed to take
+    // int street = roundState->street;  // 0, 3, 4, or 5 representing pre-flop,
+    // flop, turn, or river respectively auto myCards =
+    // roundState->hands[active];  // your cards auto boardCards =
+    // roundState->deck;  // the board cards int myPip =
+    // roundState->pips[active];  // the number of chips you have contributed to
+    // the pot this round of betting int oppPip = roundState->pips[1-active]; //
+    // the number of chips your opponent has contributed to the pot this round
+    // of betting int myStack = roundState->stacks[active];  // the number of
+    // chips you have remaining int oppStack = roundState->stacks[1-active];  //
+    // the number of chips your opponent has remaining int continueCost = oppPip
+    // - myPip;  // the number of chips needed to stay in the pot int
+    // myContribution = STARTING_STACK - myStack;  // the number of chips you
+    // have contributed to the pot int oppContribution = STARTING_STACK -
+    // oppStack;  // the number of chips your opponent has contributed to the
+    // pot char myBounty = roundState->bounty[active];  // your current bounty
+    // rank if (legalActions.find(Action::Type::RAISE) != legalActions.end()) {
+    //   auto raiseBounds = roundState->raiseBounds();  // the smallest and
+    //   largest numbers of chips for a legal bet/raise int minCost =
+    //   raiseBounds[0] - myPip;  // the cost of a minimum bet/raise int maxCost
+    //   = raiseBounds[1] - myPip;  // the cost of a maximum bet/raise
     // }
-    if (legalActions.find(Action::Type::CHECK) != legalActions.end()) {  // check-call
+    if (legalActions.find(Action::Type::CHECK) !=
+        legalActions.end()) { // check-call
       return {Action::Type::CHECK};
     }
     return {Action::Type::CALL};
