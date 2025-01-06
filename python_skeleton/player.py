0ec14dfa46a20be6a1214ecaf4a44ff8241b2d73
jacob-david <127282874+jacob-david@users.noreply.github.com>
2025-01-06 13:49:19 -0600

    Update player.py to remove typo

--- python_skeleton/player.py
+++ python_skeleton/player.py
@@ -64,7 +64,7 @@ class Player(Bot):
         #street = previous_state.street  # 0, 3, 4, or 5 representing when this round ended
         #my_cards = previous_state.hands[active]  # your cards
         #opp_cards = previous_state.hands[1-active]  # opponent's cards or [] if not revealed
-        #opponent_bounty = teriminal_state.bounty_hits # True if opponent hit bounty
+        #opponent_bounty = terminal_state.bounty_hits # True if opponent hit bounty
 
     def get_action(self, game_state, round_state, active):
         '''
