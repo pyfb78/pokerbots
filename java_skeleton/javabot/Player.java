9e6bad582b29c52c638f69c50512825a4019ed61
bossbobster <bobby.kostin@gmail.com>
2025-01-06 17:20:39 -0800

    fixed skeletons more

--- java_skeleton/javabot/Player.java
+++ java_skeleton/javabot/Player.java
@@ -34,11 +34,11 @@ public class Player implements Bot {
      * @param active Your player's index.
      */
     public void handleNewRound(GameState gameState, RoundState roundState, int active) {
-        //int myBankroll = gameState.bankroll;  // the total number of chips you've gained or lost from the beginning of the game to the start of this round
-        //float gameClock = gameState.gameClock;  // the total number of seconds your bot has left to play this game
-        //int roundNum = gameState.roundNum;  // the round number from 1 to State.NUM_ROUNDS
-        //List<String> myCards = roundState.hands.get(active);  // your cards
-        //boolean bigBlind = (active == 1);  // true if you are the big blind
+        // int myBankroll = gameState.bankroll;  // the total number of chips you've gained or lost from the beginning of the game to the start of this round
+        // float gameClock = gameState.gameClock;  // the total number of seconds your bot has left to play this game
+        // int roundNum = gameState.roundNum;  // the round number from 1 to State.NUM_ROUNDS
+        // List<String> myCards = roundState.hands.get(active);  // your cards
+        // boolean bigBlind = (active == 1);  // true if you are the big blind
     }
 
     /**
@@ -49,13 +49,13 @@ public class Player implements Bot {
      * @param active Your player's index.
      */
     public void handleRoundOver(GameState gameState, TerminalState terminalState, int active) {
-        //int myDelta = terminalState.deltas.get(active);  // your bankroll change from this round
-        //RoundState previousState = (RoundState)(terminalState.previousState);  // RoundState before payoffs
-        //int street = previousState.street;  // 0, 3, 4, or 5 representing when this round ended
-        //List<String> myCards = previousState.hands.get(active);  // your cards
-        //List<String> oppCards = previousState.hands.get(1-active);  // opponent's cards or "" if not revealed
-        //Boolean myBountyHit = terminalState.bounty_hits.get(active);  // if your bounty hit this round
-        //Boolean oppBountyHit = terminalState.bounty_hits.get(1-active);  // if opponent's bounty hit this round
+        // int myDelta = terminalState.deltas.get(active);  // your bankroll change from this round
+        // RoundState previousState = (RoundState)(terminalState.previousState);  // RoundState before payoffs
+        // int street = previousState.street;  // 0, 3, 4, or 5 representing when this round ended
+        // List<String> myCards = previousState.hands.get(active);  // your cards
+        // List<String> oppCards = previousState.hands.get(1-active);  // opponent's cards or "" if not revealed
+        // Boolean myBountyHit = terminalState.bounty_hits.get(active);  // if your bounty hit this round
+        // Boolean oppBountyHit = terminalState.bounty_hits.get(1-active);  // if opponent's bounty hit this round
     }
 
     /**
@@ -69,22 +69,22 @@ public class Player implements Bot {
      */
     public Action getAction(GameState gameState, RoundState roundState, int active) {
         Set<ActionType> legalActions = roundState.legalActions();  // the actions you are allowed to take
-        //int street = roundState.street;  // 0, 3, 4, or 5 representing pre-flop, flop, turn, or river respectively
-        //List<String> myCards = roundState.hands.get(active);  // your cards
-        //List<String> boardCards = roundState.deck;  // the board cards
-        //int myPip = roundState.pips.get(active);  // the number of chips you have contributed to the pot this round of betting
-        //int oppPip = roundState.pips.get(1-active);  // the number of chips your opponent has contributed to the pot this round of betting
-        //int myStack = roundState.stacks.get(active);  // the number of chips you have remaining
-        //int oppStack = roundState.stacks.get(1-active);  // the number of chips your opponent has remaining
-        //Character myBounty = roundState.bounties.get(active);  // your current bounty rank
-        //int continueCost = oppPip - myPip;  // the number of chips needed to stay in the pot
-        //int myContribution = State.STARTING_STACK - myStack;  // the number of chips you have contributed to the pot
-        //int oppContribution = State.STARTING_STACK - oppStack;  // the number of chips your opponent has contributed to the pot
-        //if (legalActions.contains(ActionType.RAISE_ACTION_TYPE)) {
+        // int street = roundState.street;  // 0, 3, 4, or 5 representing pre-flop, flop, turn, or river respectively
+        // List<String> myCards = roundState.hands.get(active);  // your cards
+        // List<String> boardCards = roundState.deck;  // the board cards
+        // int myPip = roundState.pips.get(active);  // the number of chips you have contributed to the pot this round of betting
+        // int oppPip = roundState.pips.get(1-active);  // the number of chips your opponent has contributed to the pot this round of betting
+        // int myStack = roundState.stacks.get(active);  // the number of chips you have remaining
+        // int oppStack = roundState.stacks.get(1-active);  // the number of chips your opponent has remaining
+        // Character myBounty = roundState.bounties.get(active);  // your current bounty rank
+        // int continueCost = oppPip - myPip;  // the number of chips needed to stay in the pot
+        // int myContribution = State.STARTING_STACK - myStack;  // the number of chips you have contributed to the pot
+        // int oppContribution = State.STARTING_STACK - oppStack;  // the number of chips your opponent has contributed to the pot
+        // if (legalActions.contains(ActionType.RAISE_ACTION_TYPE)) {
         //    List<Integer> raiseBounds = roundState.raiseBounds();  // the smallest and largest numbers of chips for a legal bet/raise
         //    int minCost = raiseBounds.get(0) - myPip;  // the cost of a minimum bet/raise
         //    int maxCost = raiseBounds.get(1) - myPip;  // the cost of a maximum bet/raise
-        //}
+        // }
         if (legalActions.contains(ActionType.CHECK_ACTION_TYPE)) {  // check-call
             return new Action(ActionType.CHECK_ACTION_TYPE);
         }
