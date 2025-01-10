e61eaff6d148bbf73454ccc88499d309f9b59033
Pavan Yeddanapudi <pavan@dhcp-10-29-172-63.dyn.MIT.EDU>
2025-01-10 11:46:47 -0500

    Getting rid of the unnecessary files

--- bot1/example.py
+++
@@ -1,9 +0,0 @@
-import holdem_calc
-import time
-
-start_time = time.time()
-print(holdem_calc.calculate(["As", "Ks", "Jd"], False, 10000, None, ["8s", "7s", "?", "?"], False))
-end_time = time.time()
-
-elapsed_time = end_time - start_time
-print(f"Elapsed time: {elapsed_time:.4f} seconds")
